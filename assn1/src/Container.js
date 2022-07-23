b104a54837cbf745a591fa0c0a38163ede5b2285
AnCha <tory912@gmail.com>
2022-07-24 07:38:48 +0900

    modified

--- assn1/src/Container.js
+++ assn1/src/Container.js
@@ -1,9 +1,19 @@
-const Container = ({children}) => {
-    return (
-        <div style={{marginLeft:40, padding:22, width: 200,borderRadius: 20 ,border:"1px solid gray", backgroundColor: "lightgray", boxShadow: "2px 2px 2px 1px gray"}}>
-            {children}
-        </div>
-    );
+const Container = ({ children }) => {
+  return (
+    <div
+      style={{
+        marginLeft: 40,
+        padding: 22,
+        width: 200,
+        borderRadius: 20,
+        border: "1px solid gray",
+        backgroundColor: "lightgray",
+        boxShadow: "2px 2px 2px 1px gray",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
